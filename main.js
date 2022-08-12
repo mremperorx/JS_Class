022476e1024655b0a897bc67da82fbfbf26aa68b
Majed El-Naser <Majedalnasr7@gmail.com>
2022-08-12 03:52:48 +0300

    JavaScript Project

---
+++ main.js
@@ -0,0 +1,14 @@
+const brandname = "Mustang";
+const wheels = 4;
+const max_speed = 300;
+const in_on = false;
+const wheels_material = "aluminum";
+const countryoforigin = "USA";
+
+const input = prompt("Scrie ceva")
+
+console.log(input)
+
+console.log(typeof input)
+
+const now = new Date();
